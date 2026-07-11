5ab8cc9f5606201865076be06609b3158ecb27fe
ahmad <ahmad.im.iseed@gmail.com>
2026-07-11 13:05:28 +0300

    updated return message

--- src/index.js
+++ src/index.js
@@ -60,7 +60,7 @@ redisClient.connect();
 // Save data into Redis
 app.get('/', async (req, res) => {
   await redisClient.set('product', 'product...');
-  res.send('<h1>Hello</h1>');
+  res.send('<h1>Hello tresmerge from AWS </h1>');
 });
 
 // Read data from Redis
